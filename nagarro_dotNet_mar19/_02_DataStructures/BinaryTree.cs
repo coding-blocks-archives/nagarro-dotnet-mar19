ddc3b77d37a86986eddc43671caa53bf805deffa
Deepak Aggarwal <daggarwal77@gmail.com>
2019-02-26 16:21:13 +0530

    binary tree

--- nagarro_dotNet_mar19/_02_DataStructures/BinaryTree.cs
+++ nagarro_dotNet_mar19/_02_DataStructures/BinaryTree.cs
@@ -27,7 +27,8 @@ namespace nagarro_dotNet_mar19
             public static void main()
             {
                 TreeNode root = createTree();
-                PrintTree(root);
+                //PrintTree(root);
+                PrintTreeLevelWise(root);
             }
 
             public static TreeNode createTree()
@@ -43,6 +44,41 @@ namespace nagarro_dotNet_mar19
                 root.right = createTree();
 
                 return root;
+            }
+
+            public static void PrintTree(TreeNode root)
+            {
+                if (root == null) return;
+
+                PrintTree(root.left);
+                Console.Write($"{root.data}, ");
+                PrintTree(root.right);
+            }
+
+            public static void PrintTreeLevelWise(TreeNode root)
+            {
+                if (root == null) return;
+
+                Queue<TreeNode> q = new Queue<TreeNode>();
+                TreeNode MARKER = null;
+
+                q.Enqueue(root);
+                q.Enqueue(MARKER);
+
+                while (q.Count != 0)
+                {
+                    TreeNode cur = q.Dequeue();
+                    if (cur == MARKER)
+                    {
+                        Console.WriteLine();
+                        if (q.Count != 0) q.Enqueue(MARKER);
+                        continue;
+                    }
+
+                    Console.Write($"{cur.data}, ");
+                    if (cur.left != null)  q.Enqueue(cur.left);
+                    if (cur.right != null) q.Enqueue(cur.right);
+                }
             }
 
         }
