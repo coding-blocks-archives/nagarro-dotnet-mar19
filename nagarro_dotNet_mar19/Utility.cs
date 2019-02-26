0770a3c82747909425cb7e8d6196667eaedbe88e
Deepak Aggarwal <daggarwal77@gmail.com>
2019-02-26 14:55:06 +0530

    utils; stkque add

---
+++ nagarro_dotNet_mar19/Utility.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace utils
+{
+    public class Utils
+    {
+        public static void PrintArr(int[] arr, int elementsToPrint)
+        {
+            for (int i = 0; i < elementsToPrint; ++i)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+
+        }
+
+        public static void Swap(ref int element1, ref int element2)
+        {
+            int tmp = element1;
+            element1 = element2;
+            element2 = tmp;
+        }
+
+        public static int[] InpArr(int nelements)
+        {
+            return null;
+        }
+
+        public static void PrintList(LinkedList<int> list)
+        {
+
+        }
+    }
+}
---
+++ nagarro_dotNet_mar19/_02_DataStructures/StkQue.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using utils;
+
+namespace nagarro_dotNet_mar19
+{
+    namespace datastructures
+    {
+        public class StkQue
+        {
+            public static void main()
+            {
+                Stack<int> s = new Stack<int>();
+                Queue<int> q = new Queue<int>();
+                LinkedList<int> l = new LinkedList<int>();
+
+                int n = int.Parse(Console.ReadLine());
+                int[] arr = utils.Utils.InpArr(n);
+                var result = NextLargestRight(arr);
+                Utils.PrintList(result);
+            }
+
+            public static LinkedList<int> NextLargestRight(int[] arr)
+            {
+                return null;
+            }
+
+
+
+        }
+    }
+}
