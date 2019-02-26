eca591997e089b472d76c348c8ffd23d543236ff
Deepak Aggarwal <daggarwal77@gmail.com>
2019-02-26 11:50:36 +0530

    linked list

---
+++ nagarro_dotNet_mar19/_02_DataStructures/LinkedList.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace nagarro_dotNet_mar19.datastructures
+{
+    class LinkedListFunctionalities
+    {
+        public static void main()
+        {
+            Node head = createLL();
+            printLL(head);
+        }
+
+        public class Node
+        {
+            public int data;
+            public Node next;
+
+            public Node()
+            {
+                data = 0;
+                next = null;
+            }
+
+            public Node(int _data)
+            {
+                data = _data;
+                next = null;
+            }
+        }
+
+        public static Node createLL()
+        {
+            Node head = null;
+            while (true)
+            {
+                int x = int.Parse(Console.ReadLine());
+                if (x == -1) break;
+                head = insertIntoLL(head, x);
+            }
+            return head;
+        }
+
+        public static Node insertIntoLL(Node head, int data)
+        {
+            Node newNode = new Node(data);
+            if (head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                Node tail = getTail(head);
+                tail.next = newNode;
+            }
+            return head;
+        }
+
+        public static Node getTail(Node head)
+        {
+            // TODO
+            return null;
+        }
+
+        public static void printLL(Node head)
+        {
+            // TODO 
+        }
+    }
+}
