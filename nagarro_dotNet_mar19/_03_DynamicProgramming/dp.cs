373d615a5f2daaf8009930a9b699fd9f17d58abe
Deepak Aggarwal <daggarwal77@gmail.com>
2019-03-08 12:32:14 +0530

    add: jumps dp

--- nagarro_dotNet_mar19/_03_DynamicProgramming/dp.cs
+++ nagarro_dotNet_mar19/_03_DynamicProgramming/dp.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace nagarro_dotNet_mar19
@@ -8,20 +9,102 @@ namespace nagarro_dotNet_mar19
     {
         class Questions
         {
+            static int[] fibArr;
+            static int[] jumparr;
+
             public static int fibonnaciRec(int n)
             {
                 if (n <= 0) return 0;
                 if (n == 1) return 1;
+                if (fibArr[n] != 0) return fibArr[n];
+
                 int prev = fibonnaciRec(n - 1);
                 int superPrev = fibonnaciRec(n - 2);
-                return prev + superPrev;
+
+                fibArr[n] = prev + superPrev;
+                return fibArr[n];
             }
+
+            public static int fibonnaciDP(int n)
+            {
+                int[] fibArr = new int[n+ 1];
+                fibArr[0] = 0;
+                fibArr[1] = 1;
+                for (int i = 2; i <= n; ++i)
+                {
+                    fibArr[i] = fibArr[i - 1] + fibArr[i - 2];
+                }
+                return fibArr[n];
+
+            }
+
+            public static int nJumpsRec(int[] potential, int startIdx)
+            {
+                if (startIdx == potential.Length) return 0;
+                if (jumparr[startIdx] != -1) return jumparr[startIdx];
+
+                int minJumps = (int)1e9;
+
+                int curPotential = potential[startIdx];
+                for(int jumpLen = 1; jumpLen <= curPotential; ++jumpLen)
+                {
+                    int landingIdx = jumpLen + startIdx;
+                    if (landingIdx > potential.Length) break;
+                    int jumpsReq = nJumpsRec(potential, landingIdx) + 1;
+                    minJumps = jumpsReq < minJumps ? jumpsReq : minJumps;
+                }
+
+                return jumparr[startIdx] = minJumps;
+            }
+
+            public static int nJumpsDP(int[] potential)
+            {
+                // this array represents the MIN number of jumps required to reach at ith box
+                // from the START
+                int[] jumparr = Enumerable.Repeat(potential.Length, (int)1e9).ToArray();
+
+                //int[] jumparr = new int[potential.Length];
+                //for (int i = 0; i < potential.Length; ++i) jumparr[i] = (int)1e9;
+
+                jumparr[0] = 0;
+
+                for(int i = 0; i < potential.Length; ++i)
+                {
+                    int maxPotential = potential[i];
+                    for(int jumpLen = 1; jumpLen < maxPotential; ++jumpLen)
+                    {
+                        int landingIdx = jumpLen + i;
+                        if (landingIdx >= potential.Length) break;
+                        jumparr[landingIdx] = Math.Min(jumparr[landingIdx], jumparr[i] + 1); 
+                    }
+                }
+                return jumparr[potential.Length - 1];
+
+            }
+
 
 
             public static void main()
             {
-                int ans = fibonnaciRec(45);
+                //int n = 35;
+                //fibArr = new int[n + 1];
+                //int ans = fibonnaciRec(n);
+                //int ans = fibonnaciDP(n);
+                //Console.WriteLine($"{ans}");
+
+                //int[] potential = { 1, 3, 5, 0, 4, 2, 6, 0, 6, 8, 9};
+                //int[] potential = { 3, 6, 7, 1, 6, 3, 5, 8, 3 };
+                //jumparr = new int[potential.Length];
+                //for (int i = 0; i < jumparr.Length; ++i)
+                //{
+                //    jumparr[i] = -1;
+                //}
+                //int ans = nJumpsRec(potential, 0);
+
+                int[] potential = { 3, 6, 7, 1, 6, 3, 5, 8, 3 };
+                int ans = nJumpsDP(potential);
                 Console.WriteLine($"{ans}");
+
             }
         }
     }
